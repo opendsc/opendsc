c5b410236deb80d5a7edc9025481bb659faf4a5d
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-06-29 15:40:12 -0500

    Fix templates

--- src/OpenDsc.Templates/templates/dsc-aot/Program.cs
+++ src/OpenDsc.Templates/templates/dsc-aot/Program.cs
@@ -2,6 +2,8 @@ using System.CommandLine;
 
 using OpenDsc.Resource.CommandLine;
 
+using Temp;
+
 var resource = new TempResource(SourceGenerationContext.Default);
 var command = CommandBuilder<TempResource, TempSchema>.Build(resource, SourceGenerationContext.Default);
 return command.Invoke(args);
