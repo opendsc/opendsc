0db6de8bea99687fa893e15931ef197bf43c85e5
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-07-22 22:09:48 -0500

    Update templates (#21)

--- src/OpenDsc.Templates/templates/dsc-aot/Resource.cs
+++ src/OpenDsc.Templates/templates/dsc-aot/Resource.cs
@@ -10,11 +10,11 @@ namespace Temp;
 [ExitCode(1, Description = "Invalid parameter")]
 [ExitCode(2, Exception = typeof(Exception), Description = "Generic error")]
 [ExitCode(3, Exception = typeof(JsonException), Description = "Invalid JSON")]
-public sealed class TempResource(JsonSerializerContext context) : AotDscResource<TempSchema>(context), IGettable<TempSchema>
+public sealed class Resource(JsonSerializerContext context) : AotDscResource<Schema>(context), IGettable<Schema>
 {
-    public TempSchema Get(TempSchema instance)
+    public Schema Get(Schema instance)
     {
-        return new TempSchema()
+        return new Schema()
         {
             Name = "Test",
             Exist = false
