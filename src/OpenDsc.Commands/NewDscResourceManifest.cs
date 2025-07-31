644ceff7c1c41636f4497c9334ad85d7e5ed6a12
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-07-31 00:25:20 -0500

    Add PowerShell module

---
+++ src/OpenDsc.Commands/NewDscResourceManifest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Thomas Nieto - All Rights Reserved
+// You may use, distribute and modify this code under the
+// terms of the MIT license.
+
+using System.Management.Automation;
+using System.Text.Json;
+
+using Json.Schema;
+using Json.Schema.Generation;
+
+namespace OpenDsc.Commands;
+
+[Cmdlet(VerbsCommon.New, "DscResourceManifest")]
+public class NewDscResourceManifest : PSCmdlet
+{
+    [Parameter(Mandatory = true)]
+    public Type Schema { get; set; } = null!;
+
+    [Parameter()]
+    public string Path { get; set; } = string.Empty;
+
+    protected override void EndProcessing()
+    {
+        var serialized = GetSchema(Schema);
+        WriteObject(serialized);
+    }
+
+    private static string GetSchema(Type type)
+    {
+        var builder = new JsonSchemaBuilder();
+        var schema = builder.FromType(type).Build();
+        return JsonSerializer.Serialize(schema);
+    }
+}
