e01de92ff912f4ae6011bb51ec8536199589449a
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-12-18 18:38:42 -0600

    Remove unnecessary JsonSerializable attributes (#31)

--- tests/TestResource.Multi/SourceGenerationContext.cs
+++ tests/TestResource.Multi/SourceGenerationContext.cs
@@ -2,10 +2,7 @@
 // You may use, distribute and modify this code under the
 // terms of the MIT license.
 
-using System.Text.Json;
 using System.Text.Json.Serialization;
-
-using OpenDsc.Resource;
 
 namespace TestResource.Multi;
 
@@ -15,16 +12,8 @@ namespace TestResource.Multi;
     UseStringEnumConverter = true,
     DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
 [JsonSerializable(typeof(FileSchema))]
-[JsonSerializable(typeof(TestResult<FileSchema>))]
-[JsonSerializable(typeof(SetResult<FileSchema>))]
 [JsonSerializable(typeof(UserSchema))]
-[JsonSerializable(typeof(TestResult<UserSchema>))]
-[JsonSerializable(typeof(SetResult<UserSchema>))]
 [JsonSerializable(typeof(ServiceSchema))]
-[JsonSerializable(typeof(TestResult<ServiceSchema>))]
-[JsonSerializable(typeof(SetResult<ServiceSchema>))]
-[JsonSerializable(typeof(DscResourceManifest))]
-[JsonSerializable(typeof(JsonElement))]
 internal partial class SourceGenerationContext : JsonSerializerContext
 {
 }
