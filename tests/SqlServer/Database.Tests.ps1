2f86f61e37a6e7b977375f93797fff50a01f983b
G.Reijn <gijsreijn@hotmail.com>
2026-01-20 05:37:25 +0100

    Check cancel issue

--- tests/SqlServer/Database.Tests.ps1
+++ tests/SqlServer/Database.Tests.ps1
@@ -13,7 +13,7 @@ BeforeDiscovery {
     $script:sqlServerAvailable = Initialize-SqlServerForTests
 }
 
-Describe 'SQL Server Database Resource' -Tag 'SqlServer' -Skip:(!$script:sqlServerAvailable) {
+Describe 'SQL Server Database Resource' -Tag 'SqlServer' -Skip:(!$script:sqlServerAvailable -or $IsLinux) {
     BeforeAll {
         . $helperScript
         
