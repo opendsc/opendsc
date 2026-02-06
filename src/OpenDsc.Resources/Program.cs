4ef6f78150f2b2f0bfd0e6974f0fc887f40ce97e
Gijs Reijn <gijsreijn@hotmail.com>
2026-02-06 01:28:13 +0100

    Add `OpenDsc.SqlServer/LinkedServer` resource (#67)
    Co-authored-by: Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>

--- src/OpenDsc.Resources/Program.cs
+++ src/OpenDsc.Resources/Program.cs
@@ -22,6 +22,7 @@ using SqlServerDatabaseRoleNs = OpenDsc.Resource.SqlServer.DatabaseRole;
 using SqlServerServerRoleNs = OpenDsc.Resource.SqlServer.ServerRole;
 using SqlServerDatabasePermissionNs = OpenDsc.Resource.SqlServer.DatabasePermission;
 using SqlServerServerPermissionNs = OpenDsc.Resource.SqlServer.ServerPermission;
+using SqlServerLinkedServerNs = OpenDsc.Resource.SqlServer.LinkedServer;
 using SqlServerConfigurationNs = OpenDsc.Resource.SqlServer.Configuration;
 using SqlServerAgentJobNs = OpenDsc.Resource.SqlServer.AgentJob;
 
@@ -55,6 +56,8 @@ var sqlServerDatabaseRoleResource = new SqlServerDatabaseRoleNs.Resource(OpenDsc
 var sqlServerServerRoleResource = new SqlServerServerRoleNs.Resource(OpenDsc.Resource.SqlServer.SourceGenerationContext.Default);
 var sqlServerDatabasePermissionResource = new SqlServerDatabasePermissionNs.Resource(OpenDsc.Resource.SqlServer.SourceGenerationContext.Default);
 var sqlServerServerPermissionResource = new SqlServerServerPermissionNs.Resource(OpenDsc.Resource.SqlServer.SourceGenerationContext.Default);
+var sqlServerLinkedServerResource = new SqlServerLinkedServerNs.Resource(OpenDsc.Resource.SqlServer.SourceGenerationContext.Default);
+
 var sqlServerConfigurationResource = new SqlServerConfigurationNs.Resource(OpenDsc.Resource.SqlServer.SourceGenerationContext.Default);
 var sqlServerAgentJobResource = new SqlServerAgentJobNs.Resource(OpenDsc.Resource.SqlServer.SourceGenerationContext.Default);
 
@@ -97,6 +100,7 @@ command.AddResource<SqlServerDatabaseRoleNs.Resource, SqlServerDatabaseRoleNs.Sc
 command.AddResource<SqlServerServerRoleNs.Resource, SqlServerServerRoleNs.Schema>(sqlServerServerRoleResource);
 command.AddResource<SqlServerDatabasePermissionNs.Resource, SqlServerDatabasePermissionNs.Schema>(sqlServerDatabasePermissionResource);
 command.AddResource<SqlServerServerPermissionNs.Resource, SqlServerServerPermissionNs.Schema>(sqlServerServerPermissionResource);
+command.AddResource<SqlServerLinkedServerNs.Resource, SqlServerLinkedServerNs.Schema>(sqlServerLinkedServerResource);
 command.AddResource<SqlServerConfigurationNs.Resource, SqlServerConfigurationNs.Schema>(sqlServerConfigurationResource);
 command.AddResource<SqlServerAgentJobNs.Resource, SqlServerAgentJobNs.Schema>(sqlServerAgentJobResource);
 
