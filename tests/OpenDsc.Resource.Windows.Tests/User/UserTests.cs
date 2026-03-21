07dfb8c3f9173e790e4188108d474669afaba640
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2026-03-21 18:31:03 -0500

    Migrate User resource tests to xUnit

---
+++ tests/OpenDsc.Resource.Windows.Tests/User/UserTests.cs
@@ -0,0 +1,189 @@
+// Copyright (c) Thomas Nieto - All Rights Reserved
+// You may use, distribute and modify this code under the
+// terms of the MIT license.
+
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+
+using AwesomeAssertions;
+
+using OpenDsc.Schema;
+
+using Xunit;
+
+using UserResource = OpenDsc.Resource.Windows.User.Resource;
+using UserSchema = OpenDsc.Resource.Windows.User.Schema;
+
+namespace OpenDsc.Resource.Windows.Tests.User;
+
+[Trait("Category", "Integration")]
+public sealed class UserTests
+{
+    private readonly UserResource _resource = new(SourceGenerationContext.Default);
+
+    private static string CreateUserName() => $"DscTestUser_{Guid.NewGuid():N}"[..16];
+
+    [Fact]
+    public void GetSchema_ReturnsValidJson()
+    {
+        var schemaJson = _resource.GetSchema();
+        var doc = System.Text.Json.JsonDocument.Parse(schemaJson);
+
+        doc.RootElement.GetProperty("$schema").GetString()
+            .Should().Be("https://json-schema.org/draft/2020-12/schema");
+    }
+
+    [Fact]
+    public void DscResourceAttribute_HasCorrectTypeAndVersion()
+    {
+        var attr = typeof(UserResource).GetCustomAttribute<OpenDsc.Resource.DscResourceAttribute>();
+
+        attr.Should().NotBeNull();
+        attr!.Type.Should().Be("OpenDsc.Windows/User");
+        attr.Version.ToString().Should().NotBeNullOrEmpty();
+    }
+
+    [WindowsOnlyFact]
+    public void Get_NonExistentUser_ReturnsExistFalse()
+    {
+        var userName = "NonExistUser_12345_UnitTest";
+
+        var result = _resource.Get(new UserSchema { UserName = userName });
+
+        result.Exist.Should().BeFalse();
+        result.UserName.Should().Be(userName);
+    }
+
+    [RequiresAdminFact]
+    public void Set_NewUser_CreatesUser()
+    {
+        var userName = CreateUserName();
+
+        try
+        {
+            _resource.Set(new UserSchema
+            {
+                UserName = userName,
+                Password = "P@ssw0rd!123",
+                FullName = "Test User Full Name",
+                Description = "Created by DSC test"
+            });
+
+            var result = _resource.Get(new UserSchema { UserName = userName });
+
+            result.UserName.Should().Be(userName);
+            result.FullName.Should().Be("Test User Full Name");
+            result.Description.Should().Be("Created by DSC test");
+            result.Exist.Should().NotBe(false);
+        }
+        finally
+        {
+            _resource.Delete(new UserSchema { UserName = userName });
+        }
+    }
+
+    [RequiresAdminFact]
+    public void Set_ExistingUser_UpdatesDescription()
+    {
+        var userName = CreateUserName();
+
+        try
+        {
+            _resource.Set(new UserSchema
+            {
+                UserName = userName,
+                Password = "P@ssw0rd!123",
+                FullName = "Initial Name",
+                Description = "Initial Description"
+            });
+
+            _resource.Set(new UserSchema
+            {
+                UserName = userName,
+                Description = "Updated Description"
+            });
+
+            var result = _resource.Get(new UserSchema { UserName = userName });
+
+            result.Description.Should().Be("Updated Description");
+        }
+        finally
+        {
+            _resource.Delete(new UserSchema { UserName = userName });
+        }
+    }
+
+    [RequiresAdminFact]
+    public void Set_UserDisabled_DisablesAccount()
+    {
+        var userName = CreateUserName();
+
+        try
+        {
+            _resource.Set(new UserSchema
+            {
+                UserName = userName,
+                Password = "P@ssw0rd!123",
+                FullName = "Temp User",
+                Description = "Temporary disabled test"
+            });
+
+            _resource.Set(new UserSchema
+            {
+                UserName = userName,
+                Disabled = true
+            });
+
+            var result = _resource.Get(new UserSchema { UserName = userName });
+
+            result.Disabled.Should().BeTrue();
+        }
+        finally
+        {
+            _resource.Delete(new UserSchema { UserName = userName });
+        }
+    }
+
+    [RequiresAdminFact]
+    public void Delete_ExistingUser_RemovesUser()
+    {
+        var userName = CreateUserName();
+
+        _resource.Set(new UserSchema
+        {
+            UserName = userName,
+            Password = "P@ssw0rd!123"
+        });
+
+        _resource.Delete(new UserSchema { UserName = userName });
+
+        var result = _resource.Get(new UserSchema { UserName = userName });
+
+        result.Exist.Should().BeFalse();
+    }
+
+    [RequiresAdminFact]
+    public void Export_NoFilter_ReturnsUsers()
+    {
+        var userName = CreateUserName();
+
+        try
+        {
+            _resource.Set(new UserSchema
+            {
+                UserName = userName,
+                Password = "P@ssw0rd!123"
+            });
+
+            var users = _resource.Export(null).ToList();
+
+            users.Should().NotBeEmpty();
+            users.Select(u => u.UserName).Should().Contain(userName);
+        }
+        finally
+        {
+            _resource.Delete(new UserSchema { UserName = userName });
+        }
+    }
+}
